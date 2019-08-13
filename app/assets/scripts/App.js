f9fb5dc45e43a6da2f71c0779bf70a84c717248e
Semir <outofmatriiks@yahoo.com>
2019-08-13 18:17:43 +0200

    Completed sticky-header.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -16,6 +16,8 @@ Jane.payTaxes();*/
 import MobilMenu from './modules/MobilMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 var mobilMenu = new MobilMenu();
  new RevealOnScroll($('.feature-item'), "85%");
  new RevealOnScroll($('.testimonial'), "60%");
+ var stickyHeader = new StickyHeader();
